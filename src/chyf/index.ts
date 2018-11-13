9f59dfd8951b3e44c4f4a302bb76e9df86b85875
davebrochu15 <43046538+davebrochu15@users.noreply.github.com>
2018-11-13 16:45:18 -0500

    feature(ChyfExtension); Add flowpathUpstream and flowpathDownstream extensions
    Add the remove holes option

--- src/chyf/index.ts
+++ src/chyf/index.ts
@@ -7,13 +7,16 @@ import Map from "api/map";
  * Global object called by the "rz-extensions".
  * The fonction "init" contain the global map instance "api"
  */
-(<any>window).myExtension = {
+(<any>window).chyf = {
     init: function(api: Map) {
         const manageExtension: ExtensionsManager = new ExtensionsManager(api, "CHyF");
-        const upstream: Extension = new CHyFExtension("upstream",`http://dev.geogratis.gc.ca:8012/chyf/drainageArea/upstreamOf.json`);
-        const downstream: Extension = new CHyFExtension("downstream",`http://dev.geogratis.gc.ca:8012/chyf/drainageArea/downstreamOf.json`);
+        const flowpathUpstream: Extension = new CHyFExtension(api, "Flowpath Upstream", "http://dev.geogratis.gc.ca:8012/chyf/eflowpath/upstreamOf.json");
+        const flowpathDownstream: Extension = new CHyFExtension(api, "Flowpath Downstream", "http://dev.geogratis.gc.ca:8012/chyf/eflowpath/downstreamOf.json");
+        const upstream: Extension = new CHyFExtension(api, "Upstream","http://dev.geogratis.gc.ca:8012/chyf/drainageArea/upstreamOf.json");
+        const downstream: Extension = new CHyFExtension(api, "Downstream","http://dev.geogratis.gc.ca:8012/chyf/drainageArea/downstreamOf.json");
 
-        manageExtension.addExtensions([upstream, downstream]);
+        manageExtension.addHTMLComponent(`<div class="list-extensions">Remove Holes: <input id="removeHoles" type="checkbox" value="holes"></div>`);
+        manageExtension.addExtensions([upstream, downstream, flowpathUpstream, flowpathDownstream]);
     }
 };
 
